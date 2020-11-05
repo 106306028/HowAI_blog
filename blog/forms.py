c994dd3d71f1feea5b87b80cb42b38234239a5ee
106306028 <106306028@nccu.edu.tw>
2020-11-05 13:59:51 +0800

    first commit

---
+++ blog/forms.py
@@ -0,0 +1,37 @@
+from django import forms
+from django.db.models import ObjectDoesNotExist
+from ckeditor.widgets import CKEditorWidget
+from .models import Blog, BlogType
+
+class BlogForm(forms.Form):
+    title = forms.CharField(
+        label='文章標題', 
+        widget=forms.TextInput(
+            attrs={'class':'form-control', 'placeholder':'請輸入標題'}
+        )
+    )
+    blog_type = forms.ModelChoiceField(
+        label='文章分類',
+        queryset=BlogType.objects.all(),
+        to_field_name="type_name",
+        widget=forms.Select(
+            attrs={'class':'form-control', 'placeholder':'請選擇分類'}
+        )
+    )
+    content = forms.CharField(
+        label='內文',
+        widget=CKEditorWidget(config_name='blog_ckeditor'),
+                            error_messages={'required':'內容不得為空白!'}
+    )
+    def __init__(self, *args, **kwargs):
+        if 'user' in kwargs:
+            self.user = kwargs.pop('user')
+        super(BlogForm, self).__init__(*args, **kwargs)
+
+    def clean(self):
+        #if user authenticated
+        if self.user.is_authenticated:
+            self.cleaned_data['user'] = self.user
+        else:
+            raise forms.ValidationError('用戶尚未登入')
+        return self.cleaned_data
---
+++ myblog/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings #導入settings
+from django.conf.urls.static import static #導入static
+from . import views
+
+urlpatterns = [
+    path('', views.home, name = 'home'),
+    path('admin/', admin.site.urls),
+    path('ckeditor',include('ckeditor_uploader.urls')), #引入路徑
+    path('blog/', include('blog.urls')), #導入應用中設置的路徑
+    path('comment/',include('comment.urls')),
+    path('likes/',include('likes.urls')),
+    path('user/', include('user.urls')),
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) #導入路徑
